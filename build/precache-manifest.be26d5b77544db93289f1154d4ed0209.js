48d0d2dd89362d61e7a4b79fdd88620e83c87fa9
foii-mudeii <59875787+foii-mudeii@users.noreply.github.com>
2020-01-14 11:45:43 -0300

    Monitor upload

---
+++ build/precache-manifest.be26d5b77544db93289f1154d4ed0209.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897dfd66ec9f068b1e1d9e9ea7b8b279",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12b626adb18bcdb0518e",
+    "url": "./static/css/main.2bfe23fa.chunk.css"
+  },
+  {
+    "revision": "edf0c5247f383d1948a5",
+    "url": "./static/js/2.5c005882.chunk.js"
+  },
+  {
+    "revision": "12b626adb18bcdb0518e",
+    "url": "./static/js/main.a80c981a.chunk.js"
+  },
+  {
+    "revision": "0e66f7b6bd03c0563a2d",
+    "url": "./static/js/runtime-main.89931c20.js"
+  },
+  {
+    "revision": "ec49b0de1ed835871400c90b499c1cdd",
+    "url": "./static/media/dashboard1.ec49b0de.svg"
+  },
+  {
+    "revision": "b45640e1accbafb3cfe7c894dbeaaac9",
+    "url": "./static/media/edit.b45640e1.svg"
+  },
+  {
+    "revision": "cbb751d5a370292b397a3ebc33e5f3da",
+    "url": "./static/media/gavel.cbb751d5.svg"
+  },
+  {
+    "revision": "0047b0416ba4baf23919d78b6d709d82",
+    "url": "./static/media/logout.0047b041.svg"
+  },
+  {
+    "revision": "87fd827e725228f0c46421db6faefeee",
+    "url": "./static/media/money.87fd827e.svg"
+  },
+  {
+    "revision": "a7ff6d4938cb2717f4ee3575546c17a1",
+    "url": "./static/media/mudeii_branco.a7ff6d49.svg"
+  },
+  {
+    "revision": "008505efd97531c782102121896c8343",
+    "url": "./static/media/mudeii_horizontal.008505ef.svg"
+  },
+  {
+    "revision": "dde5cd7200f90374dc1d953c360bb0ce",
+    "url": "./static/media/speed.dde5cd72.svg"
+  },
+  {
+    "revision": "8f41a226c41440ba2bdda00623fac57a",
+    "url": "./static/media/usuarios.8f41a226.svg"
+  }
+]);
